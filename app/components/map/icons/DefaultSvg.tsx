90fb1648d9a8dfc225d8ccdb5baa1b664ec6fccb
Philipp <p.eistrach@gmail.com>
2022-07-04 21:11:38 +0200

    Add unit svgs

---
+++ app/components/map/icons/DefaultSvg.tsx
@@ -0,0 +1,8 @@
+export default function DefaultSvg() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 86.6">
+      <polygon points="25.29 86.1 0.58 43.3 25.29 0.5 74.71 0.5 99.42 43.3 74.71 86.1 25.29 86.1" />
+      <path d="M74.42,1,98.85,43.3,74.42,85.6H25.58L1.15,43.3,25.58,1H74.42M75,0H25L0,43.3,25,86.6H75l25-43.3L75,0Z" />
+    </svg>
+  );
+}
