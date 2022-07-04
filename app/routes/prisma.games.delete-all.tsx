ad4d26173b9f2ebd8bfa91d84685b24dfadb85c2
Philipp <p.eistrach@gmail.com>
2022-07-04 22:39:14 +0200

    Add route to delete all games

---
+++ app/routes/prisma.games.delete-all.tsx
@@ -0,0 +1,9 @@
+import { LoaderFunction, redirect } from "@remix-run/node";
+import { prisma } from "~/db.server";
+import { requireUser } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUser(request);
+  await prisma.game.deleteMany();
+  return redirect("/games");
+};
