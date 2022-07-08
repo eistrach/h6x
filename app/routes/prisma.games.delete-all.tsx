e1b182e0a1a4c85ac3337e9e90d5861da7361641
EugenEistrach <eugen.eistrach@gmail.com>
2022-07-08 02:22:10 +0200

    get route to delete all games

--- app/routes/prisma.games.delete-all.tsx
+++ app/routes/prisma.games.delete-all.tsx
@@ -1,6 +1,12 @@
-import { ActionFunction, redirect } from "@remix-run/node";
+import { ActionFunction, LoaderFunction, redirect } from "@remix-run/node";
 import { prisma } from "~/db.server";
 import { requireUser } from "~/domain/auth/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUser(request);
+  await prisma.game.deleteMany();
+  return redirect("/games");
+};
 
 export const action: ActionFunction = async ({ request }) => {
   await requireUser(request);
